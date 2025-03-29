6b72d19f9b954b62f4f283d7f5486409100ca3f2
Shree-007 <helloshree8703@gmial.com>
2025-03-29 15:17:04 +0530

    changed migrate.ts

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -4,16 +4,17 @@ import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
 // Configure dotenv to read from a path specified in an environment variable,
-// with a fallback to '.env.local'
+// with a fallback to '.env.local' if not specified
 config({
   path: process.env.ENV_PATH || '.env.local',
 });
 
-// Hardcode the POSTGRES_URL for testing
-const POSTGRES_URL = "postgres://postgres.kyhzcamisnebvlvdixob:Shree-803no@aws-0-us-east-1.pooler.supabase.com:5432/postgres";
-
 const runMigrate = async () => {
-  const connection = postgres(POSTGRES_URL, { max: 1 });
+  if (!process.env.POSTGRES_URL) {
+    throw new Error('POSTGRES_URL is not defined');
+  }
+
+  const connection = postgres(process.env.POSTGRES_URL, { max: 1 });
   const db = drizzle(connection);
 
   console.log('⏳ Running migrations...');
