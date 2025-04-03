842a6c446a10f1f90561a3154e2b1b8d96bd3272
Samarjayee <samarjayee@gmail.com>
2025-04-03 19:20:23 +0100

    Update app-sidebar.tsx

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -7,6 +7,7 @@ import { PlusIcon } from '@/components/icons';
 import { SidebarHistory } from '@/components/sidebar-history';
 import { SidebarUserNav } from '@/components/sidebar-user-nav';
 import { Button } from '@/components/ui/button';
+import { NoCarbonLogo } from '@/components/icons';
 import {
   Sidebar,
   SidebarContent,
@@ -34,9 +35,7 @@ export function AppSidebar({ user }: { user: User | undefined }) {
               }}
               className="flex flex-row gap-3 items-center"
             >
-              <span className="text-lg font-semibold px-2 hover:bg-muted rounded-md cursor-pointer">
-                Chatbot
-              </span>
+              <NoCarbonLogo size={40} />
             </Link>
             <Tooltip>
               <TooltipTrigger asChild>
